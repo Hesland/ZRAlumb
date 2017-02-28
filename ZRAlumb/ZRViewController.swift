2c9fc9a015ef7c024428f80cdf9492a535cc7ba4
wangchenyu <ioszhaoranwang@foxmail.com>
2017-02-28 17:25:45 +0800

    ZRViewController has finished.

--- ZRAlumb/ZRViewController.swift
+++ ZRAlumb/ZRViewController.swift
@@ -12,8 +12,26 @@ class ZRViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.view.backgroundColor = UIColor.red
+        
+        self.setupUI()
+        
+    }
+    
+    private func setupUI() {
+        self.view.backgroundColor = UIColor.white
+        
+        let button = UIButton.init(type: .custom)
+        button.setTitle("点击开始选图", for: .normal)
+        button.setTitleColor(UIColor.red, for: .normal)
+        button.center = self.view.center
+        button.bounds.size = CGSize.init(width: 200, height: 30)
+        button.addTarget(self, action: #selector(zr_buttonClick), for: .touchUpInside)
+        
+        self.view.addSubview(button)
+    }
+    
+    @objc private func zr_buttonClick() {
+        print("点击完毕")
     }
 
     override func didReceiveMemoryWarning() {
