42b4f2b17575252d027f643098f9ec71e59b386d
wangchenyu <ioszhaoranwang@foxmail.com>
2017-02-28 17:13:46 +0800

    Test - remove 'ViewController ' from the project.

---
+++ Pods/Target Support Files/Pods-ZRAlumb/Pods-ZRAlumb-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZRAlumb : NSObject
+@end
+@implementation PodsDummy_Pods_ZRAlumb
+@end
---
+++ ZRAlumb/ZRViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ZRViewController.swift
+//  ZRAlumb
+//
+//  Created by Hesland on 2017/2/28.
+//  Copyright © 2017年 Hesland. All rights reserved.
+//
+
+import UIKit
+
+class ZRViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.red
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
